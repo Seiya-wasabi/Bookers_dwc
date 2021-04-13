b164cf4428bcceb2d48c25a28e8174442ae91c50
Seiya-wasabi <se.sato614@gmail.com>
2021-04-13 09:43:25 +0000

    テストのみ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :homes
 
-  get "/" => "homes#top"
+  root :to => "homes#top"
   get "/books" => "homes#index"
   post "homes" => "homes#create"
   patch "books/:id" => "books#update", as: "update_book"
